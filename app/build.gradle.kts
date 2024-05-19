081d586aeee38e7b3629ce70c872acfe4fa5cd33
NikitaDonkey <polovykhnikita@gmail.com>
2024-05-20 02:55:04 +0700

    the first step with no work and no errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -34,7 +35,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("com.github.clans:fab:1.6.4")
 }
